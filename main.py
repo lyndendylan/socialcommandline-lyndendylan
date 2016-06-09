4b693d53693c3b37aa3cee5b4d72cbcbabad6f2e
thenewkid <cyberkid108@gmail.com>
2016-06-09 05:51:47 -0400

    safety push, added twitter and Aut Controller

--- main.py
+++ main.py
@@ -14,21 +14,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "Controllers"))
+
 import webapp2
 import jinja2
-import os
 import json
 import random
 import pickle
 import string
 import re
 import logging
+import httplib2
+import oauthclient
+import oauthclient.forms
+import oauthclient.models
+import functools
+import twitter
+import TwitterController
 
-from google.appengine.ext import db
+from gaesessions import get_current_session
+from gaesessions import delete_expired_sessions
+from google.appengine.ext import ndb
 from google.appengine.api import mail
 from google.appengine.api import users
 from xml.dom import minidom
-from models import User
+
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
@@ -47,11 +62,8 @@ class MainHandler(webapp2.RequestHandler):
 
 class HomePage(MainHandler):
     def get(self):
-        if not users.get_current_user():
-            google_login_url = users.create_login_url("/")
-            self.render("homepage.html", gurl = google_login_url)
-        else:
-            self.redirect("/profile")
+        twitter_service = TwitterController.get_twitter_service()
+        self.render("homepage.html")
 
 class WhatIs(MainHandler):
     def get(self):
@@ -59,17 +71,82 @@ class WhatIs(MainHandler):
 
 class Profile(MainHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            show_info(user)
-            check_user_exists(user)
-            google_logout_url = users.create_logout_url("/")
-            self.render("profile.html", gurl = google_logout_url)
+        pass
 		
 class ShowData(MainHandler):
     def get(self):
         users = User.get_all()
         self.render("data.html", users=users)
+
+class TwitterAuthorized(MainHandler):
+    def get(self):
+        twitter_service = oauthclient.models.OAuthService.get_by_key_name("twitter")
+        verifier = self.request.get("oauth_verifier")
+        session = get_current_session()
+        key = session.get('twitter_request_key')
+        secret = session.get('twitter_request_secret')
+        if key is None or secret is None:
+            self.error(500)
+            return
+
+        key, secret = oauthclient.exchange_request_token_for_access_token(twitter_service.consumer_key,
+                                                                          twitter_service.consumer_secret,
+                                                                          twitter_service.access_token_url,
+                                                                          verifier,
+                                                                          key,
+                                                                          secret)
+
+        twitapi = twitter.Api(twitter_service.consumer_key,
+                              twitter_service.consumer_secret,
+                              key,
+                              secret,
+                              cache=None)
+
+        twituser = twitapi.VerifyCredentials()
+        profile = Profile.get_by_key_name(twituser.screen_name)
+        if profile is None:
+            profile = Profile(key_name=twituser.screen_name)
+
+        profile.twitter_access_token_key = key
+        profile.twitter_access_token_secret = secret
+        profile.save()
+        session["twitter_screen_name"] = twituser.screen_name
+        self.redirect("/profile")
+
+class Admin(MainHandler):
+    def get(self):
+        service_formset = oauthclient.forms.create_service_formset()
+        self.render("admin.html", service_formset=service_formset)
+
+    def post(self):
+        admin_formset = oauthclient.forms.create_service_formset(self.request.POST)
+        if oauthclient.forms.save_formset(admin_formset):
+            self.redirect("/")
+        else:
+            self.redirect("/admin")
+
+class SignInWithTwitter(MainHandler):
+    def get(self):
+        twitter_service = oauthclient.models.OAuthService.get_by_key_name("twitter")
+
+        key, secret = oauthclient.retrieve_service_request_token(twitter_service.request_token_url,
+                                                                 twitter_service.consumer_key,
+                                                                 twitter_service.consumer_secret)
+        session = get_current_session()
+        if session.is_active():
+            session.terminate()
+        session['twitter_request_key'] = key
+        session['twitter_request_secret'] = secret
+
+        self.redirect(oauthclient.generate_authorize_url(twitter_service.authenticate_url, key))
+
+class SignOut(MainHandler):
+    def get(self):
+        session = get_current_session()
+        if session.is_active():
+            session.terminate()
+        self.redirect("/")
+
 
 def check_user_exists(user):
     pass
@@ -82,6 +159,13 @@ app = webapp2.WSGIApplication([
     ('/', HomePage),
     ('/whatisscl', WhatIs),
     ('/profile', Profile),
-    ('/showdata', ShowData)
+    ('/showdata', ShowData),
+    ('/registertwitter', RegisterTwitter),
+    ('/admin', Admin),
+    ('/signin', SignInWithTwitter),
+    ('/twitterauthorized', TwitterAuthorized),
+    ('/signout', SignOut)
+
+
 
 ], debug=True)
